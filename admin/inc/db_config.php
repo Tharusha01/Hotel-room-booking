f23a8a92882f45da5d349411316d377f5793f268
Tharusha <74040427+Tharusha01@users.noreply.github.com>
2024-07-21 11:54:01 +0530

    tharusha updates

--- admin/inc/db_config.php
+++ admin/inc/db_config.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 
 $hname = "localhost";
 $uname = "root";
-$pass = "reshan123";
+$pass = "";
 $dbname = "booking_db";
 
 $con = mysqli_connect($hname, $uname, $pass, $dbname);
